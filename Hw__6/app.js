2f715d1e9e21534169f809b9a52e490d5bc992a5
Aruuke11 <aruukemyrzalieva@gmail.com>
2024-10-25 19:31:41 +0600

    add HW -7

--- Hw__6/app.js
+++ Hw__6/app.js
@@ -16,6 +16,12 @@ let colors = {
     summer: "summer",
     autumn: "autumn"
 };
+let backgroundImages = {
+    spring: "url(https://avatars.dzeninfra.ru/get-zen_doc/271828/pub_65b5447b98779f1cacf16043_65b545c8bb12e30bc0c17d81/scale_1200)",
+    summer: "url(https://img.goodfon.ru/wallpaper/big/3/57/badfon-tropical-drinks-coconut.webp)",
+    autumn: "url(https://static.tildacdn.com/tild3133-3463-4033-b236-666461356539/_2023-09-14_12045968.png)",
+    winter: "url(https://img.freepik.com/free-photo/christmas-celebration-with-snowman_23-2151085040.jpg)"
+};
 function monthOfSeasons(season) {
     monthContainer.innerHTML = " ";
     months[season].forEach(function(month) {
@@ -25,6 +31,7 @@ function monthOfSeasons(season) {
         monthBtn.classList.add(colors[season]);
         monthContainer.appendChild(monthBtn);
     });
+    document.body.style.backgroundImage = backgroundImages[season];
 }
 $winter.classList.add(colors.winter);
 $spring.classList.add(colors.spring);
